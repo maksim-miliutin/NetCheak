df6f7545232ff39e6ffa4e1177183819fa651779
Maksim Miliutin <maksim.milutin06@gmail.com>
2026-08-27 18:50:51 +0300

    move to typescript 7 and vitest 4

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig(
 {
-    test: { include: ['apps/api/src/**/*.spec.ts'] },
+    test: { include: ['apps/*/src/**/*.spec.ts'] },
 });
